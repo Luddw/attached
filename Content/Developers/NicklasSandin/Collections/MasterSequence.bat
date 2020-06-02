335b9190aaad2ddb19eef7fe54284c31fe1f6733
Zpoxx <64020062+Zpoxx@users.noreply.github.com>
2020-06-02 14:36:49 +0200

    Trailer
    Added some new shots and edited the master sequence for the trailer.

--- Content/Developers/NicklasSandin/Collections/MasterSequence.bat
+++ Content/Developers/NicklasSandin/Collections/MasterSequence.bat
@@ -1,1 +1,1 @@
-rv -nomb -fullscreen -noBorders -fps 30.000000 TS1.avi TS2.avi TS3.avi TS4.avi TS5.avi TS6.avi TS7.avi TS8.avi TS9.avi
+rv -nomb -fullscreen -noBorders -fps 30.000000 TS11.avi TS10.avi TS3.avi TS6.avi TS1.avi
